13cfcafc371600061628b693bfa456befdacf264
muhammadFareed <fareedalam873@gmail.com>
2021-04-01 05:16:53 +0500

    ,new changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from models import User
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)   
 
 
 #Database
@@ -15,19 +15,44 @@ app.config['MYSQL_USER'] = db["mysql_user"]
 app.config['MYSQL_PASSWORD'] = db["mysql_password"]
 app.config['MYSQL_DB'] = db["mysql_db"]
 
-db = MySQL(app)
+
+mysqlcon = MySQL(app)
 
 @app.route('/login/', methods=["POST"])
-
-
-def login():
+def login():    
     data= User().login()
     return data
+    
+def convertToBinaryData(filename):
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+        
+    return filename`
+
 
 @app.route('/new_vehicle/', methods=["POST"])
 def new_vehicle():
-    data= User().new_vehicle()
-    return data
+    # data= User().new_vehicle()
+    # return data
+    if request.method == 'POST':
+        userDetails = request.form
+        user_id = userDetails['user_id']
+        user_name = userDetails['user_name']
+        vehicle_image = request.files['vehicle_image']
+        message = userDetails['message']
+        if vehicle_image.filename != '':
+            vehicle_image.save(vehicle_image.filename)
+            formattedImage = convertToBinaryData(vehicle_image.filename)
+            print(formattedImage)
+        cur = mysqlcon.connection.cursor()
+        cur.execute(
+            "INSERT INTO input_vehicle(user_id, user_name, vehicle_image, message) VALUES(%s, %s, %s, %s)",
+            (user_id, user_name, formattedImage, message))
+        mysqlcon.connection.commit()
+        cur.close()
+        return {'success': 'success'}, 200
+    else:
+        return 'Error while adding new vehicle!', 404
 
 if __name__ == "__main__" :
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,33 +1,43 @@
 from flask import Flask, jsonify, request
 import json
 
-class User :
+def convertToBinaryData(filename):
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return filename
+
+
+class User:
     def login(self):
         req = request.get_json(force=True)
         username = req.get('username', None)
         password = req.get('password', None)
-        data = {
-            'username': username,
-            'password': password
-        }
+        data = {'username': username, 'password': password}
         return jsonify(data)
-    
-    def new_vehicle(self):
-        # req = request.get_json(force=True)
-        cplc = request.form['cplc']
-        vehicle_number = request.form['vehicle_number']
-        data = {
-            'cplc': cplc,
-            'vehicle_number': vehicle_number
-        }
-        return jsonify(data)
+
+    # def new_vehicle(self):
+    #     if request.method == 'POST':
+    #         userDetails = request.form
+    #         user_id = userDetails['user_id']
+    #         user_name = userDetails['user_name']
+    #         vehicle_image = request.files['vehicle_image']
+    #         message = userDetails['message']
+
+    #         formattedImage = convertToBinaryData(vehicle_image)
+    #         cur = db.connection.cursor()
+    #         cur.execute(
+    #             "INSERT INTO input_vehicle(user_id, user_name, message) VALUES(%s, %s, %s)",
+    #             (user_id, user_name, message))
+    #         connection.commit()
+    #         cur.close()
+    #         print(formattedImage)
+    #         return {'success': 'success'}, 200
+    #     else:
+    #         return 'Error while adding new vehicle!', 404
 
     def all_vehicles(self):
         req = request.get_json(force=True)
         cplc = req.get('cplc', None)
         vehicle_number = req.get('vehicle_number', None)
-        data = {
-            'cplc': cplc,
-            'vehicle_number': vehicle_number
-        }
+        data = {'cplc': cplc, 'vehicle_number': vehicle_number}
         return jsonify(data)
